903a35727268d84dc6633f0c30b495f1576551f0
hantao <15943286270@163.com>
2020-08-11 10:06:35 +0800

    项目初始化配置

---
+++ src/main/java/com/hunter/simpledaily/SimpledailyApplication.java
@@ -0,0 +1,13 @@
+package com.hunter.simpledaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpledailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpledailyApplication.class, args);
+	}
+
+}
